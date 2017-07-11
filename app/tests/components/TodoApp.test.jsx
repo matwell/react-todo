f6699f40daf0ce94dd92ad9101a85c9a48a500df
matwell <marcus.atwell@gmail.com>
2017-07-11 15:58:54 +0930

    Add toggle support

--- app/tests/components/TodoApp.test.jsx
+++ app/tests/components/TodoApp.test.jsx
@@ -18,6 +18,22 @@ describe('TodoApp', () => {
     todoApp.setState({todos: []});
     todoApp.handleAddTodo(todoText);
 
-    expect(todoApp.state.todos[0].todoText);
+    expect(todoApp.state.todos[0].text).toBe(todoText);
+  });
+
+  it('should toggle completed value when handleToggle called', () => {
+    var todoData = {
+      id: 11,
+      text: 'Test features',
+      completed: false
+    };
+
+    var todoApp = TestUtils.renderIntoDocument(<TodoApp/>);
+    todoApp.setState({todos: [todoData]});
+
+    expect(todoApp.state.todos[0].completed).toBe(false);
+    todoApp.handleToggle(11);
+    expect(todoApp.state.todos[0].completed).toBe(true);
+
   });
 });
