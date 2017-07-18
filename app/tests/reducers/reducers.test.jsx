0f2925173c42f201194ea3baa682679b2d9948db
matwell <marcus.atwell@gmail.com>
2017-07-18 13:53:51 +0930

    Add todos reducer and tests

--- app/tests/reducers/reducers.test.jsx
+++ app/tests/reducers/reducers.test.jsx
@@ -1,4 +1,5 @@
 const expect = require('expect');
+const df = require('deep-freeze-strict');
 
 const reducers = require('reducers');
 
@@ -10,7 +11,7 @@ describe('Reducers', () => {
         searchText: 'dog'
       };
 
-      var res = reducers.searchTextReducer('', action);
+      var res = reducers.searchTextReducer(df(''), df(action));
 
       expect(res).toEqual(action.searchText);
     });
@@ -23,9 +24,45 @@ describe('Reducers', () => {
 
       };
 
-      var res = reducers.showCompletedReducer(false, action);
+      var res = reducers.showCompletedReducer(df(false), df(action));
 
       expect(res).toEqual(true);
+    });
+  });
+
+  describe('todosReducer', () => {
+    it('should add new todo', () => {
+      var action = {
+        type: 'ADD_TODO',
+        text: 'Walk the dog'
+      };
+
+      var res = reducers.todosReducer(df([]), df(action));
+
+      expect(res.length).toEqual(1);
+      expect(res[0].text).toEqual(action.text);
+    });
+
+    it('should toggle todo', () => {
+      var todos = [
+        {
+          id: 123,
+          text: 'Walk the dog',
+          completed: true,
+          createdAt: 123,
+          completedAt: 125
+        }
+      ];
+
+      var action = {
+        type: 'TOGGLE_TODO',
+        id: 123
+      };
+
+      var res = reducers.todosReducer(df(todos), df(action));
+
+      expect(res[0].completed).toEqual(false);
+      expect(res[0].completedAt).toEqual(undefined);
     });
   });
 });
